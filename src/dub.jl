1bf8076d82b0b29b10acb691d6660c766eb03cd4
duringbug <2034298621@qq.com>
2023-10-19 16:20:49 +0800

    1.0

--- src/dub.jl
+++ src/dub.jl
@@ -1,7 +1,7 @@
 module dub
 
 function test()
-    include("factorize/cholesky.jl")
+    include("./factorize/cholesky.jl")
     cholesky_result = cholesky([4 -1 -1; -1 4.25 4.25; 1 2.75 3.5])
     println(cholesky_result)
 end
