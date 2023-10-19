028734f954eb46e027845545d467b8277f913c48
duringbug <2034298621@qq.com>
2023-10-19 17:14:52 +0800

    1.0

--- src/dub.jl
+++ src/dub.jl
@@ -1,9 +1,7 @@
 module dub
-include(joinpath(@__DIR__, "factorize", "cholesky.jl"))
-
+include("factorize/cholesky.jl")
 function test()
     cholesky_result = cholesky([4 -1 -1; -1 17//4 17//4; 1 11//4 7//2])
     println(cholesky_result)
 end
-
 end # module dub
