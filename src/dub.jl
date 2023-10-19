72be5f92503b0729eb977226761281fc50d3684a
duringbug <2034298621@qq.com>
2023-10-19 16:15:14 +0800

    1.0

--- src/dub.jl
+++ src/dub.jl
@@ -1,6 +1,9 @@
 module dub
 
-greet() = print("Hello World!")
-hello() = print("welcome to dub")
+function test()
+    include("factorize/cholesky.jl")
+    cholesky_result = cholesky([4 -1 -1; -1 4.25 4.25; 1 2.75 3.5])
+    println(cholesky_result)
+end
 
 end # module dub
