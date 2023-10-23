fd514c3a44df8b7616a29e5d1110169db3d75c17
duringbug <2034298621@qq.com>
2023-10-23 19:26:23 +0800

    QR的bug解决

--- src/test/test.jl
+++ src/test/test.jl
@@ -15,11 +15,13 @@ function test()
     println(lu.L * lu.U)
     println(lu.L * lu.U == lu.A)
     qr = QR{Float64}([
-        1.0 1.0
-        2.0 0.0
-        2.0 1.0
+        1.0 1.0 2.9
+        2.0 0.0 3.4
+        2.0 1.0 5.4
+        3.5 1.2 3.1
     ])
     println(qr.Q)
     println(qr.R)
     println(qr.Q * qr.R)
+    println(qr.Q * qr.R ≈ qr.A)
 end
